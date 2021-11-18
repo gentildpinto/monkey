0ce2cb801419e1e11b482f85b8e5a2972211886f
gentildpinto <gentil.junior.gp@gmail.com>
2021-11-18 23:53:50 +0100

    The lexer now can tokenize monkey source code

--- token/token.go
+++ token/token.go
@@ -34,3 +34,18 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var (
+	keywords = map[string]TokenType{
+		"fn":  FUNCTION,
+		"let": LET,
+	}
+)
+
+func LookupIdentifier(identifier string) TokenType {
+	if tok, ok := keywords[identifier]; ok {
+		return tok
+	}
+
+	return IDENT
+}
