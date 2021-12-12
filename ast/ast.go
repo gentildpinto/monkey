2b20fc971af2ff9912d02f473fedab06a1a61b56
gentildpinto <gentil.junior.gp@gmail.com>
2021-12-12 22:26:18 +0100

    ast and parser

---
+++ ast/ast.go
@@ -0,0 +1,47 @@
+package ast
+
+import "github.com/gentildpinto/monkey/token"
+
+type (
+	Node interface {
+		TokenLiteral() string
+	}
+
+	Statement interface {
+		Node
+		statementNode()
+	}
+
+	Expression interface {
+		Node
+		expressionNode()
+	}
+
+	Program struct {
+		Statements []Statement
+	}
+
+	Identifier struct {
+		Token token.Token
+		Value string
+	}
+
+	LetStatement struct {
+		Token token.Token
+		Name  *Identifier
+		Value Expression
+	}
+)
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+	return ""
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
